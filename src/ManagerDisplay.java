b85a5bd39449629fdb06756900e50988163137ac
Atif Chaudhry <achaudh5@slu.edu>
2015-02-19 19:22:24 -0600

    Cleaned up the code

--- src/ManagerDisplay.java
+++ src/ManagerDisplay.java
@@ -11,39 +11,35 @@ public class ManagerDisplay extends JFrame {
     }
 
     private void setupMDisplay() {
-        setUndecorated(true);                                               //\
-        Color color = UIManager.getColor("activeCaptionBorder");            // Removes Title Bar (Disable Drag)
-        getRootPane().setBorder(BorderFactory.createLineBorder(color, 4));  ///
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);                     // Close on Exit
+        // Set Window/Frame's Characteristics
         setExtendedState(JFrame.MAXIMIZED_BOTH);                            // Keep window maximized
-        setLayout(new GridLayout(2, 1));
+        setLayout(new GridLayout(2, 1));                                    // Set Layout
         setLocationRelativeTo(null);                                        // Position @ Center
+        setUndecorated(true);                                               // Remove Title Bar (Disable Drag)
         setVisible(true);                                                   // Show on Screen
         setResizable(false);                                                // Size is NOT adjustable (Always Maximized)
 
         JButton rButton = new JButton("Revenue");
-        Utilities.updateFont(rButton, .05);
         JButton cButton = new JButton("User Controls");
-        Utilities.updateFont(cButton, .05);
         JButton iButton = new JButton("Inventory");
+        Utilities.updateFont(rButton, .05);
+        Utilities.updateFont(cButton, .05);
         Utilities.updateFont(iButton, .05);
 
-        Utilities.startClock(timeAndDate);                                  // Initiate Clock Function to get Time and Date
+        Utilities.startClock(timeAndDate, "Welcome, Manager!");             // Initiate Clock Function to get Time and Date
         Utilities.updateFont(timeAndDate, .1);                              // Update font (10% of minimum screen resolution
         dayInfo.add(timeAndDate);                                           // Add Time and Date to GUI
 
         control.setLayout(new GridLayout(3,1));
-
         JPanel manButtons = new JPanel(new GridLayout());
 
         manButtons.add(rButton);
         manButtons.add(cButton);
         manButtons.add(iButton);
-
         control.add(manButtons);
-        getContentPane().add(dayInfo);
-        getContentPane().add(control);
+
+        add(dayInfo);
+        add(control);
     }
 
     public static void main(String[] args) {
