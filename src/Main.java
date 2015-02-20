80b42923a20d495f14af50079afd631ca4b8b948
Atif Chaudhry <achaudh5@slu.edu>
2015-02-19 19:19:32 -0600

    Removed 'Create HostDisplay' from TODO list

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@
 /** TODO LIST:
  *
  * MATCH cook display window to GUI design
- * CREATE host display window
  * CREATE waiter display window
  * - ALLOW manager to change Password! (via User Controls)
  * ADD 'Close' button to all windows (Due to No Title Bar)
