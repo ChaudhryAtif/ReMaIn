0d1d473464020cdfee9d5dc99fbb190b90339096
Atif Chaudhry <achaudh5@slu.edu>
2015-02-20 19:19:31 -0600

    Re-coded layout to add 'Back' and 'Quit' buttons

--- src/ManagerDisplay.java
+++ src/ManagerDisplay.java
@@ -6,6 +6,9 @@ public class ManagerDisplay extends JFrame {
     JPanel control = new JPanel();                                          // Bottom Panel
     JLabel timeAndDate = new JLabel();                                      // Dynamic Time & Date;
 
+    JButton backBtn = new JButton("Back");
+    JButton quitBtn = new JButton("Quit");
+
     public ManagerDisplay() {
         setupMDisplay();
     }
@@ -13,22 +16,52 @@ public class ManagerDisplay extends JFrame {
     private void setupMDisplay() {
         // Set Window/Frame's Characteristics
         setExtendedState(JFrame.MAXIMIZED_BOTH);                            // Keep window maximized
-        setLayout(new GridLayout(2, 1));                                    // Set Layout
         setLocationRelativeTo(null);                                        // Position @ Center
         setUndecorated(true);                                               // Remove Title Bar (Disable Drag)
         setVisible(true);                                                   // Show on Screen
         setResizable(false);                                                // Size is NOT adjustable (Always Maximized)
 
+        /** Characteristics of GBL **/
+        GridBagLayout gbl_Layout = new GridBagLayout();
+        gbl_Layout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_Layout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+        setLayout(gbl_Layout);
+
+        GridBagConstraints gbc_backBtn = new GridBagConstraints();          // Add Constraints to Back Button
+        gbc_backBtn.gridx = 0;
+        gbc_backBtn.gridy = 0;
+        gbc_backBtn.gridwidth = 1;
+        gbc_backBtn.gridheight = 1;
+        gbc_backBtn.insets = new Insets(5, 5, 5, 5);
+        Utilities.updateFont(backBtn, .02);
+        add(backBtn, gbc_backBtn);
+
+        GridBagConstraints gbc_quitBtn = new GridBagConstraints();          // Add Constraints to Quit Button
+        gbc_quitBtn.gridx = 13;
+        gbc_quitBtn.gridy = 0;
+        gbc_quitBtn.insets = new Insets(5, 5, 5, 5);
+        Utilities.updateFont(quitBtn, .02);
+        add(quitBtn, gbc_quitBtn);
+
+        Utilities.startClock(timeAndDate, "Welcome, Manager!");             // Initiate Clock Function to get Time and Date
+        Utilities.updateFont(timeAndDate, .09);                             // Update font (10% of minimum screen resolution
+        dayInfo.add(timeAndDate);
+
+        GridBagConstraints gbc_dayInfo = new GridBagConstraints();          // Add Constraints to Day Info Panel
+        gbc_dayInfo.gridx = 0;
+        gbc_dayInfo.gridy = 0;
+        gbc_dayInfo.gridwidth = 13;
+        gbc_dayInfo.gridheight = 3;
+        gbc_dayInfo.fill = GridBagConstraints.VERTICAL;
+        gbc_dayInfo.insets = new Insets(0, 0, 5, 0);
+        add(dayInfo, gbc_dayInfo);
+
         JButton rButton = new JButton("Revenue");
         JButton cButton = new JButton("User Controls");
         JButton iButton = new JButton("Inventory");
-        Utilities.updateFont(rButton, .05);
-        Utilities.updateFont(cButton, .05);
-        Utilities.updateFont(iButton, .05);
-
-        Utilities.startClock(timeAndDate, "Welcome, Manager!");             // Initiate Clock Function to get Time and Date
-        Utilities.updateFont(timeAndDate, .1);                              // Update font (10% of minimum screen resolution
-        dayInfo.add(timeAndDate);                                           // Add Time and Date to GUI
+        Utilities.updateFont(rButton, .065);
+        Utilities.updateFont(cButton, .065);
+        Utilities.updateFont(iButton, .065);
 
         control.setLayout(new GridLayout(3,1));
         JPanel manButtons = new JPanel(new GridLayout());
@@ -38,8 +71,14 @@ public class ManagerDisplay extends JFrame {
         manButtons.add(iButton);
         control.add(manButtons);
 
-        add(dayInfo);
-        add(control);
+        GridBagConstraints gbc_control = new GridBagConstraints();           // Add Constraints to Control Panel
+        gbc_control.gridx = 0;
+        gbc_control.gridy = 4;
+        gbc_control.gridwidth = 14;
+        gbc_control.gridheight = 2;
+        gbc_control.fill = GridBagConstraints.BOTH;
+        gbc_control.insets = new Insets(0, 0, 5, 5);
+        add(control, gbc_control);
     }
 
     public static void main(String[] args) {
