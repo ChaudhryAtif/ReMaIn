2039a0792fa2e39c555f19dda56fb13f5fb3a24d
Atif Chaudhry <achaudh5@slu.edu>
2015-02-20 19:14:24 -0600

    Re-coded layout to add 'Back' and 'Quit' buttons

--- src/LoginDisplay.java
+++ src/LoginDisplay.java
@@ -8,21 +8,55 @@ public class LoginDisplay extends JFrame {
     JPanel users = new JPanel(new GridLayout(2, 2));                        // Bottom Section
     JLabel timeAndDate = new JLabel();                                      // Dynamic Time & Date;
     private JButton cook, host, waiter, manager;                            // User Buttons
+
     ButtonListener click = new ButtonListener();                            // Listener for Buttons
     PasswordVerifier pwdVerifier = new PasswordVerifier();                  // Initialize PasswordVerifier Class
 
+    JButton backBtn = new JButton("Back");
+    JButton quitBtn = new JButton("Quit");
+
     public LoginDisplay() {
-        // Set Window/Frame's Characteristics
         setExtendedState(JFrame.MAXIMIZED_BOTH);                            // Keep window maximized
-        setLayout(new GridLayout(2, 1));                                    // Set Layout
         setLocationRelativeTo(null);                                        // Position @ Center
-        setUndecorated(true);                                               // Remove Title Bar (Disable Drag)
+        setUndecorated(true);                                               // Removes Title Bar (Disable Drag)
         setVisible(true);                                                   // Show on Screen
-        setResizable(false);                                                // Size is NOT adjustable (Always Maximized)
+        setResizable(false);
+
+        /** Characteristics of GBL **/
+        GridBagLayout gbl_Layout = new GridBagLayout();
+        gbl_Layout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_Layout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+        setLayout(gbl_Layout);
+
+        GridBagConstraints gbc_backBtn = new GridBagConstraints();          // Add Constraints to Back Button
+        gbc_backBtn.gridx = 0;
+        gbc_backBtn.gridy = 0;
+        gbc_backBtn.gridwidth = 1;
+        gbc_backBtn.gridheight = 1;
+        gbc_backBtn.insets = new Insets(5, 5, 5, 5);
+        Utilities.updateFont(backBtn, .02);
+        add(backBtn, gbc_backBtn);
+
+        GridBagConstraints gbc_quitBtn = new GridBagConstraints();          // Add Constraints to Quit Button
+        gbc_quitBtn.gridx = 13;
+        gbc_quitBtn.gridy = 0;
+        gbc_quitBtn.insets = new Insets(5, 5, 5, 5);
+        Utilities.updateFont(quitBtn, .02);
+        add(quitBtn, gbc_quitBtn);
 
         Utilities.startClock(timeAndDate, "Welcome!");                      // Initiate Clock Function to get Time and Date
-        Utilities.updateFont(timeAndDate, .1);                              // Update font (10% of minimum screen resolution
-        dayInfo.add(timeAndDate);                                           // Add Time and Date to GUI
+        Utilities.updateFont(timeAndDate, .11);                             // Update font (10% of minimum screen resolution
+        dayInfo.add(timeAndDate);
+
+        GridBagConstraints gbc_dayInfo = new GridBagConstraints();          // Add Constraints to Day Info Panel
+        gbc_dayInfo.gridx = 0;
+        gbc_dayInfo.gridy = 0;
+        gbc_dayInfo.gridwidth = 13;
+        gbc_dayInfo.gridheight = 3;
+        gbc_dayInfo.fill = GridBagConstraints.VERTICAL;
+        gbc_dayInfo.insets = new Insets(0, 0, 5, 0);
+        add(dayInfo, gbc_dayInfo);
+
 
         // Create buttons for users, and update font
         cook = new JButton("Cook");
@@ -40,15 +74,21 @@ public class LoginDisplay extends JFrame {
         users.add(waiter);
         users.add(manager);
 
-        // Add JPanels to JFrame
-        add(dayInfo);                                                      // Add Greeting Panel to JFrame
-        add(users);
+        GridBagConstraints gbc_users = new GridBagConstraints();           // Add Constraints to Users Panel
+        gbc_users.gridx = 0;
+        gbc_users.gridy = 3;
+        gbc_users.gridwidth = 14;
+        gbc_users.gridheight = 3;
+        gbc_users.fill = GridBagConstraints.BOTH;
+        gbc_users.insets = new Insets(0, 0, 5, 5);
+        add(users, gbc_users);
 
         // Listeners for all the Buttons;
         cook.addActionListener(click);
         host.addActionListener(click);
         waiter.addActionListener(click);
         manager.addActionListener(click);
+        quitBtn.addActionListener(click);
 
     }
 
@@ -63,7 +103,7 @@ public class LoginDisplay extends JFrame {
                 }
             }
 
-            if (event.getSource() == host) {
+            if (event.getSource() == host) {                                // If the password is correct, open HostDisplay
                 if (pwdVerifier.verifyPwd("host")) {
                     HostDisplay hDisplay = new HostDisplay();
                 }
@@ -78,6 +118,9 @@ public class LoginDisplay extends JFrame {
                 if (pwdVerifier.verifyPwd("manager")) {
                     ManagerDisplay mDisplay = new ManagerDisplay();         // If the password is correct, open ManagerDisplay
                 }
+            }
+            if (event.getSource() == quitBtn) {                             // If the quit button is pressed, quit
+                System.exit(0);
             }
         }
     }
