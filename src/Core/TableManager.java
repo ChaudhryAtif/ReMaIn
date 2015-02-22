af90c22a586e8e4e50b509e77900807464518d1b
Brennan <brennangovreau@yahoo.com>
2015-02-22 15:15:21 -0600

    Added getters and setters to TableManager.java

--- src/Core/TableManager.java
+++ src/Core/TableManager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class TableManager {
     /*
     *   Constructor
@@ -5,12 +7,23 @@ public class TableManager {
     public TableManager(int numOfTables) {
         this.numOfTables = numOfTables;
         tables = new ArrayList<Table>(numOfTables);
+		for (int i = 0; i < numOfTables; i++) {
+			tables.add(new OpenTable());
+		}
     }
     
     /*
     *   Getters
     */
     public int getNumOfTables() { return numOfTables; }
+	public ArrayList<Table> getTables() { return tables; }
+	// To Do: Make sure table numbers are lined up with indices
+	public Table getTable(int tableNumber) { return tables.get(tableNumber); }
+
+	/*
+	*	Setters
+	*/ 
+	public void setTable(int tableNumber, Table table) { tables.set(tableNumber, table); }
     
     /*
     *   Private Variables
