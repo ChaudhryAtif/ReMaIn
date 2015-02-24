b9ecab883fabe6e48a307b3d498fe1f97a10ee1b
Connor Riddell <riddell.connor.m@gmail.com>
2015-02-24 03:49:22 -0600

    LoginDisplay loos right
    Changes the GridBag so it works.
    Simpler.

--- src/LoginDisplay.java
+++ src/LoginDisplay.java
@@ -3,125 +3,127 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/** TODO LIST:
+ * MATCH cook display window to GUI design
+ * CREATE waiter display window
+ * CREATE manager display window
+ * - ALLOW manager to change Password!
+ * ADD 'Close' button to all windows (Due to No Title Bar)
+ */
+
 public class LoginDisplay extends JFrame {
-    JPanel dayInfo = new JPanel();                                          // Top Section
-    JPanel users = new JPanel(new GridLayout(2, 2));                        // Bottom Section
-    JLabel timeAndDate = new JLabel();                                      // Dynamic Time & Date;
-    private JButton cook, host, waiter, manager;                            // User Buttons
+    private JPanel dayInfo 			= new JPanel(new GridBagLayout());                          // Top Section
+    private GridBagConstraints gc 	= new GridBagConstraints();									// Location manager 
+    
+    private JPanel users			= new JPanel(new GridLayout(2, 2));                       	// Bottom Section											// 
+    private JLabel timeAndDate		= new JLabel();                                      		// Dynamic Time & Date;
 
-    ButtonListener click = new ButtonListener();                            // Listener for Buttons
-    PasswordVerifier pwdVerifier = new PasswordVerifier();                  // Initialize PasswordVerifier Class
+    private JButton cook, host, waiter, manager, quitButton;
+    ButtonListener click = new ButtonListener();                           				// Listener for Buttons
 
-    JButton backBtn = new JButton("Back");
-    JButton quitBtn = new JButton("Quit");
+    PasswordVerifier pwdVerifier = new PasswordVerifier();                  			// Initialize PasswordVerifier Class
 
     public LoginDisplay() {
-        setExtendedState(JFrame.MAXIMIZED_BOTH);                            // Keep window maximized
-        setLocationRelativeTo(null);                                        // Position @ Center
-        setUndecorated(true);                                               // Removes Title Bar (Disable Drag)
-        setVisible(true);                                                   // Show on Screen
-        setResizable(false);
+        // Set Window/Frame's Characteristics
+        super();                          									 			// Constructor
 
-        /** Characteristics of GBL **/
-        GridBagLayout gbl_Layout = new GridBagLayout();
-        gbl_Layout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-        gbl_Layout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
-        setLayout(gbl_Layout);
+        setUndecorated(true);                                               			//
+        Color color = UIManager.getColor("activeCaptionBorder");           		 		// Removes Title Bar (Disable Drag)
+        getRootPane().setBorder(BorderFactory.createLineBorder(color, 4));  			///
 
-        GridBagConstraints gbc_backBtn = new GridBagConstraints();          // Add Constraints to Back Button
-        gbc_backBtn.gridx = 0;
-        gbc_backBtn.gridy = 0;
-        gbc_backBtn.gridwidth = 1;
-        gbc_backBtn.gridheight = 1;
-        gbc_backBtn.insets = new Insets(5, 5, 5, 5);
-        Utilities.updateFont(backBtn, .02);
-        add(backBtn, gbc_backBtn);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);                            			// Keep window maximized
+        getContentPane().setLayout(new GridLayout(2, 1));                   			// Set JFrame Layout
+        setVisible(true);                                                   			// Show on Screen
+        setResizable(false);                                                			// Size is NOT adjustable (Always Maximized)
 
-        GridBagConstraints gbc_quitBtn = new GridBagConstraints();          // Add Constraints to Quit Button
-        gbc_quitBtn.gridx = 13;
-        gbc_quitBtn.gridy = 0;
-        gbc_quitBtn.insets = new Insets(5, 5, 5, 5);
-        Utilities.updateFont(quitBtn, .02);
-        add(quitBtn, gbc_quitBtn);
-
-        Utilities.startClock(timeAndDate, "Welcome!");                      // Initiate Clock Function to get Time and Date
-        Utilities.updateFont(timeAndDate, .11);                             // Update font (10% of minimum screen resolution
-        dayInfo.add(timeAndDate);
-
-        GridBagConstraints gbc_dayInfo = new GridBagConstraints();          // Add Constraints to Day Info Panel
-        gbc_dayInfo.gridx = 0;
-        gbc_dayInfo.gridy = 0;
-        gbc_dayInfo.gridwidth = 13;
-        gbc_dayInfo.gridheight = 3;
-        gbc_dayInfo.fill = GridBagConstraints.VERTICAL;
-        gbc_dayInfo.insets = new Insets(0, 0, 5, 0);
-        add(dayInfo, gbc_dayInfo);
-
+        Utilities.startClock(timeAndDate, "Welcome!");									// Initiate Clock Function to get Time and Date
+        Utilities.updateFont(timeAndDate, .11);											// Add Constraints to Day Info Panel
 
         // Create buttons for users, and update font
         cook = new JButton("Cook");
         host = new JButton("Host");
-        waiter = new JButton("Waiter");
+        waiter = new JButton("Waiter");        
         manager = new JButton("Manager");
-        Utilities.updateFont(cook, .1);
-        Utilities.updateFont(host, .1);
-        Utilities.updateFont(waiter, .1);
-        Utilities.updateFont(manager, .1);
+		Utilities.updateFont(cook, .1);
+		Utilities.updateFont(host, .1);
+		Utilities.updateFont(waiter, .1);
+		Utilities.updateFont(manager, .1);
+        
+        // All changes button color to red
+        quitButton = new JButton("Quit");  
+        
+        // Sets Font size and button size
+        Utilities.updateFont(quitButton, .05);
 
         // Add users to the JPanel
         users.add(cook);
         users.add(host);
         users.add(waiter);
         users.add(manager);
+        
+        // Add quit button to JPanel to control button size (Upper Left)
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.anchor = GridBagConstraints.NORTHWEST;
+        gc.insets = new Insets(2, 0, 0, 2);
+        dayInfo.add(quitButton, gc);
 
-        GridBagConstraints gbc_users = new GridBagConstraints();           // Add Constraints to Users Panel
-        gbc_users.gridx = 0;
-        gbc_users.gridy = 3;
-        gbc_users.gridwidth = 14;
-        gbc_users.gridheight = 3;
-        gbc_users.fill = GridBagConstraints.BOTH;
-        gbc_users.insets = new Insets(0, 0, 5, 5);
-        add(users, gbc_users);
+        // Add timeAndDate to JPanel (Center)
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.anchor = GridBagConstraints.CENTER;
+        gc.insets = new Insets(2, 0, 0, 2);
+        dayInfo.add(timeAndDate, gc);
 
         // Listeners for all the Buttons;
         cook.addActionListener(click);
         host.addActionListener(click);
         waiter.addActionListener(click);
         manager.addActionListener(click);
-        quitBtn.addActionListener(click);
-
+        quitButton.addActionListener(click);
+        
+        // Add JPanels to JFrame
+        add(dayInfo);
+        add(users);
     }
 
     /**
      * ButtonListener implementation to respond to button clicks
      */
-    public class ButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            if (event.getSource() == cook) {
-                if (pwdVerifier.verifyPwd("cook")) {                        // If the password is correct, open CookDisplay
-                    CookDisplay cDisplay = new CookDisplay();
-                }
+    private class ButtonListener implements ActionListener 
+    {
+        public void actionPerformed(ActionEvent event) 
+        {
+        	// Cook Button Clicked
+            if (event.getSource() == cook) 
+            {
+            	// If the password is correct, open cookDisplay
+                if (pwdVerifier.verifyPwd("cook"))  	{new CookDisplay();}
+            }
+            
+            // Host Button Clicked
+            if (event.getSource() == host) 
+            {
+            	// If the password is correct, open HostDisplay
+                if (pwdVerifier.verifyPwd("host"))  	{new HostDisplay(); }
             }
 
-            if (event.getSource() == host) {                                // If the password is correct, open HostDisplay
-                if (pwdVerifier.verifyPwd("host")) {
-                    HostDisplay hDisplay = new HostDisplay();
-                }
+            // Waiter Button Clicked
+            if (event.getSource() == waiter) 
+            {
+            	// If the password is correct, open WaiterDisplay
+                if (pwdVerifier.verifyPwd("waiter")) 	{System.out.println("Waiter Password Accepted!");}
             }
-
-            if (event.getSource() == waiter) {
-                if (pwdVerifier.verifyPwd("waiter")) {
-                    System.out.println("Waiter Password Accepted!");
-                }
+            
+            // Manager Button Clicked
+            if (event.getSource() == manager) 
+            {
+            	// If the password is correct, open ManagerDisplay
+                if (pwdVerifier.verifyPwd("manager")) 	{new ManagerDisplay();}
             }
-            if (event.getSource() == manager) {
-                if (pwdVerifier.verifyPwd("manager")) {
-                    ManagerDisplay mDisplay = new ManagerDisplay();         // If the password is correct, open ManagerDisplay
-                }
-            }
-            if (event.getSource() == quitBtn) {                             // If the quit button is pressed, quit
-                System.exit(0);
-            }
-        }
-    }
-}
+            
+            // Quit Button clicked
+            if (event.getSource() == quitButton) 		{System.exit(0);}
+        } // end actionPerformed
+    } // end ButtonListener
+} // end LoginDisplay
