8dc3ef8818b336e4e863097648c914b78b68a8d1
Atif Chaudhry <achaudh5@slu.edu>
2015-02-26 19:12:34 -0600

    Moved DayInfo code to Utilities

--- src/CookDisplay.java
+++ src/CookDisplay.java
@@ -1,79 +1,31 @@
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CookDisplay extends JFrame 
-{
-    private JPanel dayInfo;                 	// Top Panel
-    private JPanel bottomPane;
-    private GridBagConstraints	gc;				// Layout Manager for Gridbag
-    
-    private JPanel onCall;						
-    private JPanel inventory;
-    private JLabel timeAndDate;                 // Dynamic Time & Date;
-    private JTabbedPane tabbedPane;
+public class CookDisplay extends JFrame {
+    private JPanel bottomPane = new JPanel();
+    private GridBagConstraints	gc = new GridBagConstraints();				// Layout Manager for Gridbag
 
-    private JButton backButton;					// Button to back to Login screen
-    private JButton quitButton;					// Button to exit app
-    private ButtonListener click;				// Action listener to detect click
+    private JPanel dayInfo = new JPanel();                                          // Top Panel
+    private JPanel onCall = new JPanel();
+    private JPanel inventory = new JPanel();
+    private JLabel timeAndDate = new JLabel();                                      // Dynamic Time & Date;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+
+    private JButton backBtn = new JButton("Back");
+    private JButton quitBtn = new JButton("Quit");
+    private ButtonListener click = new ButtonListener();                            // Listener for Buttons
 
     private JLabel label = new JLabel("2nd Tab");
 
-    public CookDisplay() 	{setupCDisplay();}
+    public CookDisplay() {
+        setupCDisplay();
+    }
 
-    private void setupCDisplay() 
-    {
-    	setLayout(new GridLayout(2,1));										// Grid Layout cuts page Horizontally
-        setUndecorated(true);                                               // Removes Title Bar (Disable Drag)
-        setExtendedState(JFrame.MAXIMIZED_BOTH);                            // Keep window maximized
-        setVisible(true);                                                   // Show on Screen
-        setResizable(false);                                                // Size is NOT adjustable (Always Maximized)
-        
-        // Initialize private Variables
-        dayInfo 		= new JPanel(new GridBagLayout());					// Initializes the Gridbag layout
-        bottomPane		= new JPanel();
-        gc				= new GridBagConstraints();
-        onCall 			= new JPanel();
-        inventory 		= new JPanel();
-        timeAndDate		= new JLabel(); 
-        tabbedPane		= new JTabbedPane();
-        click			= new ButtonListener();
-        label			= new JLabel("2nd Tab");
-        
-        Utilities.startClock(timeAndDate, "Welcome, Cook!");                // Initiate Clock Function to get Time and Date
-        Utilities.updateFont(timeAndDate, .09);                             // Update font (10% of minimum screen resolution
-        
-        // Initialize Back button
-        backButton 		= new JButton("Back");
-        Utilities.updateFont(backButton, .05);								// set font size larger
-        
-        // Add back button to JPanel to control button size (Upper Left)
-        gc.gridx = 0;
-        gc.gridy = 0;
-        gc.anchor = GridBagConstraints.NORTHWEST;
-        gc.insets = new Insets(2, 0, 0, 2);
-        dayInfo.add(backButton, gc);
-        
-        // Initialize quit button
-        quitButton 		= new JButton("Quit");
-        Utilities.updateFont(quitButton, .05); 								// Set font size larger
-        
-        // Add quit button to JPanel to control button size (Upper Right)
-        gc.gridx = 0;
-        gc.gridy = 0;
-        gc.anchor = GridBagConstraints.NORTHEAST;
-        gc.insets = new Insets(2, 0, 0, 2);
-        dayInfo.add(quitButton, gc);
-
-        // Add timeAndDate to JPanel (Center)
-        gc.gridx = 0;
-        gc.gridy = 0;
-        gc.anchor = GridBagConstraints.CENTER;
-        gc.insets = new Insets(2, 0, 0, 2);
-        dayInfo.add(timeAndDate, gc);
-
+    private void setupCDisplay() {
+        // Add Back and Quit Buttons, as well as Time and Date
+        Utilities.startDayInfo(this, dayInfo, backBtn, quitBtn, "Welcome, Cook!", timeAndDate, .09, 3, false);
 
         JTable orderTable;
         Object[][] orderData = {
@@ -85,47 +37,51 @@ public class CookDisplay extends JFrame
         orderTable = new JTable(orderData, colNames);
         orderTable.setLayout(new FlowLayout());
 
-        // Set the table to be as big as possible on screen
-        orderTable.setPreferredScrollableViewportSize(new Dimension(Utilities.getSWidth()-35, (Utilities.getSHeight()/2)-87));
-        
-        orderTable.setFillsViewportHeight(true);											
+        // Maximize table on screen
+        orderTable.setPreferredScrollableViewportSize(new Dimension(Utilities.getSWidth()-30, (Utilities.getSHeight()/2)+70));
+        orderTable.setFillsViewportHeight(true);
         Utilities.updateFont(orderTable, .02);												// Make font bigger
         orderTable.setRowHeight(30);														// Make rows of table bigger
 
         onCall.add(new JScrollPane(orderTable));											// Allows table to scroll
 
         inventory.add(label);
-        
+
         tabbedPane.setFont(new Font("SansSerif", Font.BOLD, 15));
         tabbedPane.add("<html><body leftmargin=15 topmargin=18 marginwidth=15 marginheight=15>OnCall</body></html>", onCall);
         tabbedPane.add("<html><body leftmargin=15 topmargin=18 marginwidth=15 marginheight=15>Inventory</body></html>", inventory);
-        
-        bottomPane.add(tabbedPane);
-        
+
+        GridBagConstraints gbc_tPane = new GridBagConstraints();            // Add Constraints to Control Panel
+        gbc_tPane.gridx = 0;
+        gbc_tPane.gridy = 3;
+        gbc_tPane.gridwidth = 14;
+        gbc_tPane.gridheight = 3;
+        gbc_tPane.fill = GridBagConstraints.BOTH;
+        gbc_tPane.insets = new Insets(0, 5, 5, 5);
+
         // Add action Listeners to buttons
-        quitButton.addActionListener(click);
-        backButton.addActionListener(click);
-        
-        // Add top and bottom to JFrame
-        add(dayInfo);
-        add(bottomPane);
+        backBtn.addActionListener(click);
+        quitBtn.addActionListener(click);
+        add(tabbedPane, gbc_tPane);
     }
-    
+
     /**
      * ButtonListener implementation to respond to button clicks
      */
-    private class ButtonListener implements ActionListener 
+    private class ButtonListener implements ActionListener
     {
-        public void actionPerformed(ActionEvent event) 
+        public void actionPerformed(ActionEvent event)
         {
             // Quit Button clicked
-            if (event.getSource() == quitButton) 		{System.exit(0);}
-            
-            // Back button clicked
-            if (event.getSource() == backButton)	 	{setVisible(false);}
-        } // end actionPerformed
-    } // end ButtonListener
+            if (event.getSource() == quitBtn) 		{System.exit(0);}
 
-    public static void main(String[] args) {new CookDisplay();}
+            // Back button clicked
+            if (event.getSource() == backBtn)	 	{setVisible(false);}
+        }
+    }
+
+    public static void main(String[] args) {
+        CookDisplay cDisplay = new CookDisplay();
+    }
 
 }
