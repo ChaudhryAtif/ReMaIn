42340b5d9468b7fe5293918ca076d1d05e5abbb3
Atif Chaudhry <achaudh5@slu.edu>
2015-02-26 19:16:05 -0600

    Moved DayInfo code to Utilities; Updated Btn Font

--- src/LoginDisplay.java
+++ src/LoginDisplay.java
@@ -3,127 +3,85 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/** TODO LIST:
- * MATCH cook display window to GUI design
- * CREATE waiter display window
- * CREATE manager display window
- * - ALLOW manager to change Password!
- * ADD 'Close' button to all windows (Due to No Title Bar)
- */
-
 public class LoginDisplay extends JFrame {
-    private JPanel dayInfo 			= new JPanel(new GridBagLayout());                  // Top Section
-    private GridBagConstraints gc 	= new GridBagConstraints();							// Location manager 
-    
-    private JPanel users			= new JPanel(new GridLayout(2, 2));                 // Bottom Section											// 
-    private JLabel timeAndDate		= new JLabel();                                     // Dynamic Time & Date;
+    private JPanel dayInfo = new JPanel();                                  // Top Section
+    private JPanel users = new JPanel(new GridLayout(2, 2));                // Bottom Section
+    private JLabel timeAndDate = new JLabel();                              // Dynamic Time & Date;
+    private JButton cook, host, waiter, manager;                            // User Buttons
 
-    private JButton cook, host, waiter, manager, quitButton;
-    ButtonListener click = new ButtonListener();                           				// Listener for Buttons
+    private ButtonListener click = new ButtonListener();                    // Listener for Buttons
+    private PasswordVerifier pwdVerifier = new PasswordVerifier();          // Initialize PasswordVerifier Class
 
-    private PasswordVerifier pwdVerifier = new PasswordVerifier();                // Initialize PasswordVerifier Class
+    private JButton quitBtn = new JButton("Quit");
 
     public LoginDisplay() {
-        // Set Window/Frame's Characteristics
-        super();                          									 			// Constructor
-
-        setUndecorated(true);                                               			//
-        Color color = UIManager.getColor("activeCaptionBorder");           		 		// Removes Title Bar (Disable Drag)
-        getRootPane().setBorder(BorderFactory.createLineBorder(color, 4));  			///
-
-        setExtendedState(JFrame.MAXIMIZED_BOTH);                            			// Keep window maximized
-        getContentPane().setLayout(new GridLayout(2, 1));                   			// Set JFrame Layout
-        setVisible(true);                                                   			// Show on Screen
-        setResizable(false);                                                			// Size is NOT adjustable (Always Maximized)
-
-        Utilities.startClock(timeAndDate, "Welcome!");									// Initiate Clock Function to get Time and Date
-        Utilities.updateFont(timeAndDate, .11);											// Add Constraints to Day Info Panel
+        /** Add Quit Button, as well as Time and Date **/
+        Utilities.startDayInfo(this, dayInfo, null, quitBtn, "Welcome!", timeAndDate, .11, 3, true);
 
         // Create buttons for users, and update font
         cook = new JButton("Cook");
         host = new JButton("Host");
-        waiter = new JButton("Waiter");        
+        waiter = new JButton("Waiter");
         manager = new JButton("Manager");
-		Utilities.updateFont(cook, .1);
-		Utilities.updateFont(host, .1);
-		Utilities.updateFont(waiter, .1);
-		Utilities.updateFont(manager, .1);
-        
-        // All changes button color to red
-        quitButton = new JButton("Quit");  
-        
-        // Sets Font size and button size
-        Utilities.updateFont(quitButton, .05);
+        Utilities.updateFont(cook, .1);
+        Utilities.updateFont(host, .1);
+        Utilities.updateFont(waiter, .1);
+        Utilities.updateFont(manager, .1);
 
         // Add users to the JPanel
         users.add(cook);
         users.add(host);
         users.add(waiter);
         users.add(manager);
-        
-        // Add quit button to JPanel to control button size (Upper Left)
-        gc.gridx = 0;
-        gc.gridy = 0;
-        gc.anchor = GridBagConstraints.NORTHWEST;
-        gc.insets = new Insets(2, 0, 0, 2);
-        dayInfo.add(quitButton, gc);
 
-        // Add timeAndDate to JPanel (Center)
-        gc.gridx = 0;
-        gc.gridy = 0;
-        gc.anchor = GridBagConstraints.CENTER;
-        gc.insets = new Insets(2, 0, 0, 2);
-        dayInfo.add(timeAndDate, gc);
+        GridBagConstraints gbc_users = new GridBagConstraints();           // Add Constraints to Users Panel
+        gbc_users.gridx = 0;
+        gbc_users.gridy = 3;
+        gbc_users.gridwidth = 14;
+        gbc_users.gridheight = 3;
+        gbc_users.fill = GridBagConstraints.BOTH;
+        gbc_users.insets = new Insets(0, 0, 5, 5);
+        add(users, gbc_users);
 
         // Listeners for all the Buttons;
         cook.addActionListener(click);
         host.addActionListener(click);
         waiter.addActionListener(click);
         manager.addActionListener(click);
-        quitButton.addActionListener(click);
-        
-        // Add JPanels to JFrame
-        add(dayInfo);
-        add(users);
+        quitBtn.addActionListener(click);
+
     }
 
     /**
      * ButtonListener implementation to respond to button clicks
      */
-    private class ButtonListener implements ActionListener 
-    {
-        public void actionPerformed(ActionEvent event) 
-        {
-        	// Cook Button Clicked
-            if (event.getSource() == cook) 
-            {
-            	// If the password is correct, open cookDisplay
-                if (pwdVerifier.verifyPwd("cook"))  	{new CookDisplay();}
-            }
-            
-            // Host Button Clicked
-            if (event.getSource() == host) 
-            {
-            	// If the password is correct, open HostDisplay
-                if (pwdVerifier.verifyPwd("host"))  	{new HostDisplay(); }
+    public class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            if (event.getSource() == cook) {
+                if (pwdVerifier.verifyPwd("cook")) {                        // If the password is correct, open CookDisplay
+                    CookDisplay cDisplay = new CookDisplay();
+                }
             }
 
-            // Waiter Button Clicked
-            if (event.getSource() == waiter) 
-            {
-            	// If the password is correct, open WaiterDisplay
-                if (pwdVerifier.verifyPwd("waiter")) 	{System.out.println("Waiter Password Accepted!");}
+            if (event.getSource() == host) {                                // If the password is correct, open HostDisplay
+                if (pwdVerifier.verifyPwd("host")) {
+                    HostDisplay hDisplay = new HostDisplay();
+                }
             }
-            
-            // Manager Button Clicked
-            if (event.getSource() == manager) 
-            {
-            	// If the password is correct, open ManagerDisplay
-                if (pwdVerifier.verifyPwd("manager")) 	{new ManagerDisplay();}
+
+            if (event.getSource() == waiter) {
+                if (pwdVerifier.verifyPwd("waiter")) {
+                    System.out.println("Waiter Password Accepted!");
+                }
             }
-            
-            // Quit Button clicked
-            if (event.getSource() == quitButton) 		{System.exit(0);}
-        } // end actionPerformed
-    } // end ButtonListener
-} // end LoginDisplay
+            if (event.getSource() == manager) {
+                if (pwdVerifier.verifyPwd("manager")) {
+                    ManagerDisplay mDisplay = new ManagerDisplay();         // If the password is correct, open ManagerDisplay
+                }
+            }
+            if (event.getSource() == quitBtn) {                             // If the quit button is pressed, quit
+                System.exit(0);
+            }
+        }
+    }
+}
