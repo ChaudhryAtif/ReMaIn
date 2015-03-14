0428dd3f1ec02e52870ed88000f37b4007fb1782
Atif Chaudhry <achaudh5@slu.edu>
2015-03-14 15:36:29 -0500

    Added TODO comments/suggestions for User/Password
    - Added comments to create user class (somewhat created)
    - Added comment to encrypt passwords
    - Moved back/quit functionality to Utilities

--- src/LoginDisplay.java
+++ src/LoginDisplay.java
@@ -4,52 +4,47 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginDisplay extends JFrame {
-    private JPanel dayInfo = new JPanel();                                  // Top Section
-    private JPanel users = new JPanel(new GridLayout(2, 2));                // Bottom Section
-    private JLabel timeAndDate = new JLabel();                              // Dynamic Time & Date;
+    private JPanel dayInfo = new JPanel();                                  // Top Half of Frame
+    private JPanel users = new JPanel(new GridLayout(2, 2));                // Bottom Half of Frame
+    private JLabel timeDate = new JLabel();                                 // Dynamic Time & Date;
     private JButton cook, host, waiter, manager;                            // User Buttons
 
-    private ButtonListener click = new ButtonListener();                    // Listener for Buttons
-    private PasswordVerifier pwdVerifier = new PasswordVerifier();          // Initialize PasswordVerifier Class
-
-    private JButton quitBtn = new JButton("Quit");
+    private ButtonListener click = new ButtonListener();
+    private PasswordVerifier pwdVerifier = new PasswordVerifier();
 
     public LoginDisplay() {
         /** Add Quit Button, as well as Time and Date **/
-        Utilities.startDayInfo(this, dayInfo, null, quitBtn, "Welcome!", timeAndDate, .11, 3, true);
+        Utilities.startDayInfo(this, dayInfo, "Welcome!", timeDate, .11, true);
+        // TODO: Check for secured password file, if none, initiate users (cook, etc)
+        // TODO: Move passwords to a secure file                            // Pass Encryptor: http://goo.gl/dxfSrn
 
-        // Create buttons for users, and update font
-        cook = new JButton("Cook");
-        host = new JButton("Host");
-        waiter = new JButton("Waiter");
-        manager = new JButton("Manager");
-        Utilities.updateFont(cook, .1);
-        Utilities.updateFont(host, .1);
-        Utilities.updateFont(waiter, .1);
-        Utilities.updateFont(manager, .1);
+        // Create instances of User Class
+        User cook_user = new User("Cook","cooks");
+        //TODO add to user list, or dictionary, key = typeofuser, value = dict of the users where key = id_no and value = user
+        User host_user = new User("Host","hosts");
+        User waiter_user = new User("Waiter","waiters");
+        User manager_user = new User("Manager","manages");
 
-        // Add users to the JPanel
-        users.add(cook);
-        users.add(host);
-        users.add(waiter);
-        users.add(manager);
+        // Create and Update buttons
+        cook = new JButton(cook_user.getJob());
+        host = new JButton(host_user.getJob());
+        waiter = new JButton(waiter_user.getJob());
+        manager = new JButton(manager_user.getJob());
+        Utilities.multiUpdateFont(.1, cook, host, waiter, manager);
 
-        GridBagConstraints gbc_users = new GridBagConstraints();           // Add Constraints to Users Panel
-        gbc_users.gridx = 0;
-        gbc_users.gridy = 3;
-        gbc_users.gridwidth = 14;
-        gbc_users.gridheight = 3;
-        gbc_users.fill = GridBagConstraints.BOTH;
-        gbc_users.insets = new Insets(0, 0, 5, 5);
-        add(users, gbc_users);
+        // Add Buttons (users) to Users Panel, and Users to Frame
+        Utilities.multiAdd(users, cook, host, waiter, manager);
+        add(users, BorderLayout.CENTER);
 
-        // Listeners for all the Buttons;
+        // Add Listeners for all the Buttons;
         cook.addActionListener(click);
         host.addActionListener(click);
         waiter.addActionListener(click);
         manager.addActionListener(click);
-        quitBtn.addActionListener(click);
 
+        /** Set Visible Last To Avoid Glitches/Flickering **/
+        setVisible(true);
+        setResizable(false);                                                // Size is NOT adjustable (Always Maximized)
     }
 
     /**
@@ -58,30 +53,25 @@ public class LoginDisplay extends JFrame {
     public class ButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             if (event.getSource() == cook) {
-                if (pwdVerifier.verifyPwd("cook")) {                        // If the password is correct, open CookDisplay
+//                if (pwdVerifier.verifyPwd("cook")) {                        // If the password is correct, open Display
                     CookDisplay cDisplay = new CookDisplay();
-                }
+//                }
             }
-
-            if (event.getSource() == host) {                                // If the password is correct, open HostDisplay
-                if (pwdVerifier.verifyPwd("host")) {
+            if (event.getSource() == host) {
+//                if (pwdVerifier.verifyPwd("host")) {
                     HostDisplay hDisplay = new HostDisplay();
-                }
+//                }
             }
-
             if (event.getSource() == waiter) {
-                if (pwdVerifier.verifyPwd("waiter")) {
-                    System.out.println("Waiter Password Accepted!");
-                }
+//                if (pwdVerifier.verifyPwd("waiter")) {
+                    WaiterDisplay wDisplay = new WaiterDisplay();
+//                }
             }
             if (event.getSource() == manager) {
-                if (pwdVerifier.verifyPwd("manager")) {
-                    ManagerDisplay mDisplay = new ManagerDisplay();         // If the password is correct, open ManagerDisplay
-                }
-            }
-            if (event.getSource() == quitBtn) {                             // If the quit button is pressed, quit
-                System.exit(0);
+//                if (pwdVerifier.verifyPwd("manager")) {
+                    ManagerDisplay mDisplay = new ManagerDisplay();
+//                }
             }
         }
-    }
+    } // ButtonListener
 }
