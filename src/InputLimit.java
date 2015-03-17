b826b61422ace95b1d27ff11c374f60a3875374f
Atif Chaudhry <achaudh5@slu.edu>
2015-03-17 16:50:51 -0500

    Added Class to Limit Textfield Input

---
+++ src/InputLimit.java
@@ -0,0 +1,22 @@
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+/**
+ * Class to limit input length of JPasswordFields
+ */
+public class InputLimit extends PlainDocument {
+    private int maxLimit = 10;
+
+    InputLimit(int maxLimit) {
+        this.maxLimit = maxLimit;
+    }
+
+    public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
+        if (str == null) { return; }
+
+        if ((getLength() + str.length()) <= maxLimit) {    // If the combined length is <= maxLimit
+            super.insertString(offset, str, attr);
+        }
+    }
+}
