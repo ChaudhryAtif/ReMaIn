9dcd325c5d8f2e5ef6bca3fa33f4b30f7310442a
Brennan Govreau <brennangovreau@yahoo.com>
2015-03-22 14:53:47 -0500

    Update InventoryItem

--- src/InventoryItem.java
+++ src/InventoryItem.java
@@ -2,26 +2,32 @@ public class InventoryItem {
     /*
     *   Constructor
     */
-    public InventoryItem(String item, int quantityNeeded, int currentStock, String notes) {
+    public InventoryItem(int ID, String item, int quantityNeeded, int currentStock, String notes, String status) {
         this.item = item;
         this.quantityNeeded = quantityNeeded;
         this.currentStock = currentStock;
         this.notes = notes;
+        this.ID = ID;
+        this.status = status;
     }
     
     /*
     *   Getters
     */
+    public int getID() { return ID; }
     public String getItem() { return item; }
     public int getQuantityNeeded() { return quantityNeeded; }
     public int getCurrentStock() { return currentStock; }
     public String getNotes() { return notes; }
+    public String getStatus() { return status; }
     
     /*
     *   Private Variables
     */
+    private int ID;
     private String item;
     private int quantityNeeded;
     private int currentStock;
     private String notes;
+    private String status;
 }
