e166429ff99ccd6e7aff4ed8db8c8c9de3d6c281
Govreau <govreau@ubuntu.(none)>
2015-03-22 19:45:33 -0500

    Remove compareTo and time input error checking from Order.java

--- src/Order.java
+++ src/Order.java
@@ -9,30 +9,9 @@ public class Order extends Exception {
         this.orderInfo = orderInfo;
         this.tableNum = tableNum;
         this.waiter = waiter;
-        if (!(Character.isDigit(timePlaced.charAt(0))) || !(Character.isDigit(timePlaced.charAt(1))) || !(timePlaced.charAt(2) == ':') || !(Character.isDigit(timePlaced.charAt(3))) || !(Character.isDigit(timePlaced.charAt(4))))
-            throw new InvalidTimeException("Invalid Time Submitted: Use military time in form: 99:99");
-        else
-            this.timePlaced = timePlaced;
+        this.timePlaced = timePlaced;
         this.heads = heads;
         this.additionalInfo = additionalInfo;
-    }
-    
-    public static Comparator<Order> comparatorByTable() {
-        return new
-            Comparator<Order>() {
-                public int compare(Order order1, Order order2) {
-                    return order1.compareTo(order2);
-                }
-            };
-    }
-    
-    public static Comparator<Order> comparatorByTime() {
-        return new
-            Comparator<Order>() {
-                public int compare(Order order1, Order order2) {
-                    return order1.compareTo(order2);
-                }
-            };
     }
     
     /*
@@ -50,10 +29,11 @@ public class Order extends Exception {
     *    Private Variables
     */
     private int orderID;             // ID of the order
-    private String orderInfo;       // String that represents the order information
-    private int tableNum;           // Table associated with the order
-    private String waiter;          // Waiter associated with the order
-    private String timePlaced;      // Time of the order (Requires military time in form 99:99)
-    private int heads;              // Number of customers in the order
+    private String orderInfo;        // String that represents the order information
+    private int tableNum;            // Table associated with the order
+    private String status;           // Status of the order
+    private String waiter;           // Waiter associated with the order
+    private String timePlaced;       // Time of the order (Requires military time in form 99:99)
+    private int heads;               // Number of customers in the order
     private String additionalInfo;   // Additional information about the order
 }
