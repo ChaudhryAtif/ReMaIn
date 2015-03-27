31b72a3ad157327ab1f5013e886ccc8b899eca3a
Atif Chaudhry <achaudh5@slu.edu>
2015-03-27 14:47:23 -0500

    Updated TODO list. Check pass file (1st launch)

--- src/Remain.java
+++ src/Remain.java
@@ -1,19 +1,23 @@
+import java.io.File;
 
 /** TODO LIST:
  *
- * ALL TABLES: Auto-Increment IDs
- * FIRST LAUNCH: Manager User Creation Required (then others - Auto)
- * - Set passwords for self, and other users
  * LOGIN: Create User Class
- * - Encrypt Password & Store on Separate File
+ * ALL TABLES: Auto-Increment IDs
  * WAITER: Work on FoodStatus Tab
  * HOST: Validate reservation input before saving
+ *
+ * #LATER:
  * MANAGER: Allow user's password change (via User Controls)
  *
  */
 
 class Remain {
     public static void main(String[] args) {
-        new LoginDisplay();
+        File uPwd = new File("UserPass.txt");
+        if (uPwd.exists() && !uPwd.isDirectory())
+            new LoginDisplay();
+        else
+            new LaunchDisplay();
     }
 }
