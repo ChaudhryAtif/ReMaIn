1f0294ff900d60d1221c0308ae18cc2ccc81f1da
Atif Chaudhry <achaudh5@slu.edu>
2015-04-29 14:09:10 -0500

    Uncommented launch display

--- src/ReMaIn.java
+++ src/ReMaIn.java
@@ -13,10 +13,10 @@ import java.io.File;
 
 class Remain {
     public static void main(String[] args) {
-//        File uPwd = new File("UserPass.txt");
-//        if (uPwd.exists() && !uPwd.isDirectory())
+        File uPwd = new File("UserPass.txt");
+        if (uPwd.exists() && !uPwd.isDirectory())
             new LoginDisplay();
-//        else
-//            new LaunchDisplay();
+        else
+            new LaunchDisplay();
     }
 }
