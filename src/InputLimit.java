58734b31eac3cc5086ef7b3acb6c20d23f9a6827
Atif Chaudhry <achaudh5@slu.edu>
2015-04-30 12:14:06 -0500

    Minor code update

--- src/InputLimit.java
+++ src/InputLimit.java
@@ -8,14 +8,12 @@ import javax.swing.text.PlainDocument;
 public class InputLimit extends PlainDocument {
     private int maxLimit = 10;
 
-    InputLimit(int maxLimit) {
-        this.maxLimit = maxLimit;
-    }
+    InputLimit(int maxLimit) { this.maxLimit = maxLimit; }
 
     public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
         if (str == null) { return; }
 
-        if ((getLength() + str.length()) <= maxLimit) {    // If the combined length is <= maxLimit
+        if ((getLength() + str.length()) <= maxLimit) {    // If combined length is <= maxLimit
             super.insertString(offset, str, attr);
         }
     }
