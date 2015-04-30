91f17e4377c8d712ee16106ce7bf02900866e9ef
Atif Chaudhry <achaudh5@slu.edu>
2015-04-30 12:26:26 -0500

    Cleaned up the code
    Removed commented code (not in use); Removed unnecessary comments; Removed TODO list

--- src/ReMaIn.java
+++ src/ReMaIn.java
@@ -1,16 +1,5 @@
 import java.io.File;
 
-/** TODO LIST:
- *
- * LOGIN: Create User Class
- * ALL TABLES: Auto-Increment IDs
- * HOST: Validate reservation input before saving
- *
- * #LATER:
- * MANAGER: Allow user's password change (via User Controls)
- *
- */
-
 class Remain {
     public static void main(String[] args) {
         File uPwd = new File("UserPass.txt");
