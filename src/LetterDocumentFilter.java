b7e99b3c711d7bed822d49ba80c9e06d795668d7
Atif Chaudhry <achaudh5@slu.edu>
2015-04-30 12:18:41 -0500

    Cleaned up the code
    Removed commented code (not in use); Removed unnecessary comments

--- src/LetterDocumentFilter.java
+++ src/LetterDocumentFilter.java
@@ -8,32 +8,26 @@ import javax.swing.text.DocumentFilter;
 class LetterDocumentFilter extends DocumentFilter {
 
     @Override
-    public void replace(FilterBypass fb, int i, int i1, String string, AttributeSet as) throws BadLocationException 
-    {
+    public void replace(FilterBypass fb, int i, int i1, String string, AttributeSet as) throws BadLocationException {
     	/*
     	 * An inserted string may be more than a single character i.e a copy and paste of 'aaa123d', 
        	 * also we iterate from the back as super.XX implementation will put last insterted string first 
          * and so on thus 'aa123d' would be 'daa', but because we iterate from the back its 'aad' like we want
     	 */
-        for (int n = string.length(); n > 0; n--) 
-        {
-            char c = string.charAt(n - 1);           			//get a single character of the string
-            if (Character.isAlphabetic(c) || c == ' ') 			//if its an alphabetic character or white space
-                super.replace(fb, i, i1, String.valueOf(c), as);			//allow update to take place for the given character
+        for (int n = string.length(); n > 0; n--) {
+            char c = string.charAt(n - 1);           			            // Get a single character of the string
+            if (Character.isAlphabetic(c) || c == ' ') 			            // If its an alphabet letter or white space
+                super.replace(fb, i, i1, String.valueOf(c), as);			// Allow update to take place for given letter
         } // end for loop
     }
 
     @Override
-    public void remove(FilterBypass fb, int i, int i1) throws BadLocationException 
-    {
+    public void remove(FilterBypass fb, int i, int i1) throws BadLocationException {
         super.remove(fb, i, i1);
     }
 
     @Override
-    public void insertString(FilterBypass fb, int i, String string, AttributeSet as) throws BadLocationException 
-    {
+    public void insertString(FilterBypass fb, int i, String string, AttributeSet as) throws BadLocationException {
         super.insertString(fb, i, string, as);
-
     }
 }
-
